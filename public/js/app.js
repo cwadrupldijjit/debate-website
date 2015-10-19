9a917717e0b95552bf935cbce42c70ff4f258c62
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-10-19 07:51:59 -0600

    Added Angular-UI-Router setup instead of normal Angular-Router

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,18 @@
 /// <reference path="../../typings/angularjs/angular.d.ts" />
-// / <reference path="../../typings/" />
-var app = angular.module('MainApp', ['ngRoute', 'ngMaterial', 'perfectParallax']);
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-            templateUrl: '../views/home/home-template.html',
-            controller: 'HomeController',
-            controllerAs: 'vm'
-        })
-            .otherwise({ redirectTo: '/' });
-    }]);
+var app = angular.module('MainApp', ['ngRoute', 'ui.router', 'ngMaterial', 'perfectParallax']);
+app.config(['$routeProvider', '$stateProvider', '$urlRouterProvider', '$locationProvider', function ($routeProvider, $stateProvider, $urlRouterProvider, $locationProvider) {
+    // $routeProvider
+    // 	.when('/', {
+    // 		templateUrl: '../views/home/home-template.html',
+    // 		controller: 'HomeController',
+    // 		controllerAs: 'vm'
+    // 	})
+    // 	.otherwise({redirectTo: '/'});
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: '../views/home/home-template.html',
+        controller: 'HomeController',
+        controllerAs: 'vm'
+    });
+    $urlRouterProvider.otherwise('/');
+}]);
--- public/js/app.ts
+++ public/js/app.ts
@@ -1,14 +1,23 @@
 /// <reference path="../../typings/angularjs/angular.d.ts" />
-// / <reference path="../../typings/" />
 
-var app = angular.module('MainApp', ['ngRoute', 'ngMaterial', 'perfectParallax']);
+var app = angular.module('MainApp', ['ngRoute', 'ui.router', 'ngMaterial', 'perfectParallax']);
 
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-	$routeProvider
-		.when('/', {
+app.config(['$routeProvider', '$stateProvider', '$urlRouterProvider', '$locationProvider', function($routeProvider, $stateProvider, $urlRouterProvider, $locationProvider) {
+	// $routeProvider
+	// 	.when('/', {
+	// 		templateUrl: '../views/home/home-template.html',
+	// 		controller: 'HomeController',
+	// 		controllerAs: 'vm'
+	// 	})
+	// 	.otherwise({redirectTo: '/'});
+	
+	$stateProvider
+		.state('home', {
+			url: '/',
 			templateUrl: '../views/home/home-template.html',
 			controller: 'HomeController',
 			controllerAs: 'vm'
-		})
-		.otherwise({redirectTo: '/'});
+		});
+	
+	$urlRouterProvider.otherwise('/');
 }]);
