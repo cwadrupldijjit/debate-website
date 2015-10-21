70f1386db410c0e4d0d359d235fcdf6d7eda7129
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-10-21 00:00:00 -0600

    Dummy service with dummy data gave back dummy response

--- public/views/login/LoginController.js
+++ public/views/login/LoginController.js
@@ -1,7 +1,21 @@
 /* global app */
 // testing to see if user authentication worked.
-app.controller('LoginController', [function() {
+app.controller('LoginController', ['LoginService', function(LoginService) {
 	var vm = this;
 	
 	vm.test = 'test';
+	
+	vm.login_local = function() {
+		var result = LoginService.login_local({
+			username: vm.username,
+			password: vm.password
+		});
+		
+		if (!result) {
+			return console.log(result);
+		}
+		
+		vm.currentUser = result;
+		console.log('this worked');
+	}
 }]);
--- public/views/login/LoginService.js
+++ public/views/login/LoginService.js
@@ -1,3 +1,36 @@
 app.service('LoginService', [function() {
+	var serv = this;
 	
+	serv.users = [
+		{
+			username: 'test',
+			password: 'test',
+			id: '1234567',
+			avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/UserAvatar.svg/2000px-UserAvatar.svg.png'
+		}
+	];
+	
+	serv.login_local = function(loginData) {
+		if (!loginData) {
+			console.log('No login data submitted');
+			return null;
+		}
+		
+		if (loginData) {
+			for (var i = 0; i < serv.users.length; i++) {
+				if (serv.users[i].username === loginData.username) {
+					if (serv.users[i].username === loginData.password) {
+						console.log('user authenticated successfully');
+						return serv.users[i];
+					}
+					console.log('Password doesn\'t match');
+					return null;
+				}
+			}
+			
+			console.log('no user data is found for the username provided');
+			
+			return null;
+		}
+	};
 }]);
