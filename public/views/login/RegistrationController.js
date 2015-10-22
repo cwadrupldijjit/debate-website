608d6123447feea61aef97744ce2f2f517408575
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-10-21 22:06:13 -0600

    Update to Registration page

--- public/views/login/RegistrationController.js
+++ public/views/login/RegistrationController.js
@@ -9,7 +9,26 @@ app.controller('RegisterController', ['LoginService', function(LoginService) {
 			password: vm.newPassword,
 			confirmPassword: vm.confirmPassword
 		}).then(function(result) {
-			console.log(result);
+			// notificationBubble(result.msg);
+			if (result.data.data === false) {
+				vm.reg_step = 2;
+			}
 		});
+	};
+	
+	vm.takeFromOAuth = function() {
+		
+	};
+	
+	vm.verify_step2 = function() {
+		if (!vm.newFirstName && !vm.newLastName && !vm.newEmail) {
+			console.log('You need to fill in the form before you submit it.');
+		} else if ( ((vm.newFisrtName && vm.newLastName) || vm.OAuth_data) && vm.newEmail ) {
+			vm.reg_step = 3;
+		}
+	};
+	
+	vm.verify_step3 = function() {
+		// if
 	};
 }]);
