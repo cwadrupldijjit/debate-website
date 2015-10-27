913e554ca8a9539eb506665a03f32dcdbc0600b5
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-10-27 01:05:50 -0600

    More optimizations and styling updates to account view and dashboards

--- public/views/account/AccountController.js
+++ public/views/account/AccountController.js
@@ -1,5 +1,68 @@
 app.controller('AccountController', ['LoginService', '$state', function(LoginService, $state) {
 	var vm = this;
 	
+	vm.tabNum = 1;
+	vm.currentUser;
+	vm.tempUser = {};
+	vm.tempEmail = '';
 	
+	vm.isSelected = function(tabNum) {
+		if (tabNum === vm.tabNum) {
+			return "selected-tab";
+		}
+	};
+	
+	vm.setTab = function(num) {
+		vm.tabNum = num;
+	};
+	
+	function getCurrentUser() {
+		LoginService.getUser()
+			.then(function(result) {
+				console.log(result);
+				vm.currentUser = result;
+				vm.tempUser = result;
+			})
+	};
+	
+	getCurrentUser();
+	
+	vm.standardBio = "This person has no life. Otherwise, they would add their bio here.";
+	
+	vm.editModeOn = {
+		username: false,
+		email: false,
+		bio: false,
+		facebookUrl: false,
+		twitterUrl: false,
+		linkedinUrl: false
+	};
+	
+	vm.editItem = function(elem) {
+		//function that when called will set the editModeOn value for the particular item
+		switch(elem) {
+			case 'username': vm.editModeOn.username = true; vm.tempEmail += vm.tempUser.username; break;
+			case 'email': vm.editModeOn.email = true;  vm.tempEmail += vm.tempUser.email; break;
+			case 'bio': vm.editModeOn.bio = true; break;
+			case 'facebookUrl': vm.editModeOn.facebookUrl = true; break;
+			case 'twitterUrl': vm.editModeOn.twitterUrl = true; break;
+			case 'linkedinUrl': vm.editModeOn.linkedinUrl = true; break;
+		}
+	};
+	
+	vm.cancelEdit = function(elem) {
+		if (elem === 'email') {
+			vm.editModeOn.email = false;
+			
+			vm.tempUser.email = vm.currentUser.email.slice();
+		}
+	};
+	
+	vm.updateEmail = function() {
+		
+		// POSSIBLE:  confirm the user wants to do this
+		// call the $updateemail service in angularfire
+		// update it in the users array as well
+		
+	};
 }]);
